66455c990ad44a9f2d2d2b96cc677b2b453ed4ec
faouz-dev <faouzanekouko*@gmail.com>
2026-04-02 00:28:38 +0000

    fix for vercel

--- src/webapp/app.ts
+++ src/webapp/app.ts
@@ -12,15 +12,15 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", path.join(process.cwd(), "src", "webapp", "views"));
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/search", query);
+
 async function main() {
   await waitForMongooseInstancesConnected([INDEXER_DB, CRAWLER_BD]);
   console.log("mongoose connected");
-
-  app.get("/", (req, res) => {
-    res.render("home");
-  });
-
-  app.get("/search", query);
 
   app.listen(process.env.PORT ?? 3000, () => {
     console.log("Web app started");
