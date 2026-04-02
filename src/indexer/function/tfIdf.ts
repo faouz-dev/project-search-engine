aeaba96eb6b6c5ddf97b5008c1c48ccb2516826b
ZynXx <techigwe@gmail.com>
2026-04-02 19:49:38 +0100

    Update tfIdf.ts

--- src/indexer/function/tfIdf.ts
+++ src/indexer/function/tfIdf.ts
@@ -8,17 +8,15 @@
  *
  * @description Le TF-IDF (de l'anglais term frequency-inverse document frequency) est une méthode de pondération souvent utilisée en recherche d'information et en particulier dans la fouille de textes. Cette mesure statistique permet d'évaluer l'importance d'un terme contenu dans un document, relativement à une collection ou un corpus. Le poids augmente proportionnellement au nombre d'occurrences du mot dans le document. Il varie également en fonction de la répartition du mot dans le corpus. Des variantes de la formule originale sont souvent utilisées dans des moteurs de recherche pour apprécier la pertinence d'un document en fonction des critères de recherche de l'utilisateur. <source: WIKIPEDIA>
  */
+// David :)
+
 export function tfIdf(
   word_occurence: number,
   page_words_count: number,
   docsWithWord: number,
   totalDocs: number,
-): number {
-  // TF
+  ): number {
   const tf = word_occurence / page_words_count;
-
-  // IDF
-  const idf = Math.log(totalDocs + 1 / (docsWithWord + 1));
-
+  const idf = Math.log((totalDocs + 1) / (docsWithWord + 1)); // erreur legere de mathematique 🤧
   return tf * idf;
 }
